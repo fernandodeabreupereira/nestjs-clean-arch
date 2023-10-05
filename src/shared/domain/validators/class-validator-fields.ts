7141aecb84d740a799d5c9e438abbfb3ccc2ae4b
Fernando Pereira <fernandodeabreupereira@gmail.com>
2023-10-05 16:52:47 -0300

    feat: adding abstract class ClassValidatorFields

---
+++ src/shared/domain/validators/class-validator-fields.ts
@@ -0,0 +1,21 @@
+import { validateSync } from 'class-validator';
+import { FieldsErrors, IValidatorFields } from './validator-fields.interface';
+
+export abstract class ClassValidatorFields<PropsValidated> implements IValidatorFields<PropsValidated>  {
+  errors: FieldsErrors = null;
+  validatedData: PropsValidated = null;
+  validate (data: any): boolean {
+    const errors = validateSync(data);
+
+    if (errors.length) {
+      errors.forEach(error => {
+        const field = error.property;
+        this.errors[field] = Object.values(error.constraints);
+      });
+      return false;
+    }
+
+    this.validatedData = data;
+    return true;
+  }
+}
